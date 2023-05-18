630f341855d08dabdada5b136df799d5f5f44b0a
unknown <ffnasif@gmail.com>
2023-05-18 18:33:26 +0600

    completed ErrorPage component

--- src/pages/shared/ErrorPage.jsx
+++ src/pages/shared/ErrorPage.jsx
@@ -1,7 +1,23 @@
+import Lottie from 'lottie-react';
+import { Link, useRouteError } from 'react-router-dom';
+import err from '../../assets/error.json';
+
 const ErrorPage = () => {
+	const { error } = useRouteError();
+
 	return (
 		<>
-			<h1>ErrorPage</h1>
+			<div className='w-full h-full flex justify-center items-center'>
+				<Lottie className='max-w-3xl' animationData={err} loop={true} />
+			</div>
+			<p className='text-xl md:text-2xl font-semibold text-center -mt-32 md:-mt-72'>
+				{error?.message}
+			</p>
+			<div className='text-center my-10'>
+				<button className='bttn squeeze'>
+					<Link to='/'>Back to Homepage</Link>
+				</button>
+			</div>
 		</>
 	);
 };
